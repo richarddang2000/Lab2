130494464309a4c69253df1e5bce0b03445f02a7
richarddang2000 <rnd0318@tamu.edu>
2021-03-05 01:22:54 -0600

    final tests

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -8,19 +8,23 @@ RSpec.describe 'Book', type: :system do
 
 
 	#unit testing
+	
 	#book model unit test
-	it 'checks that book is createable and valid' do
-		@book = Book.new(title: "Frankenstein", author: "Mary Shelley")
+	it 'checks that book entry is created and in the database' do
+		@book = Book.new(title: "Frankenstein", author: "Mary Shelley", genre: "fiction", 
+						 price: "2.99", publishDate: "2000")
 		@book.save
 		
-		visit "books/index"
-		sleep(3)	
+		visit books_index_path
+		sleep(4)	
 		expect(@book).to be_valid
 	end
 	
 	
+	
 	#integration testing 
-	#tess integration of create page and index page by
+	
+	#test integration of create page and index page by
 	#indicating if flash triggered by create page shows 
 	#up on index page
 	
@@ -34,28 +38,35 @@ RSpec.describe 'Book', type: :system do
 	
 	
 	
+	
 	#acceptance testing
-	#implements the 'edit a book' user requirement
-	#'as a user, I would like to be able to edit 
+	
+	#implements the 'delete a book' user requirement
+	#'as a user, I would like to be able to delete 
 	# a book in the collection'
 	it 'edits a book' do
-		@book = Book.new(title: "Romeo and Juliet", author: "Shakespeare") #creates entry with title and author only
+		@book = Book.new(title: "Romeo and Juliet", author: "Shakespeare", genre: "fiction",
+						 price: "10.99", publishDate: "2000") #creates entry with title and author only
 		@book.save
 		
-		visit "books/#{@book.id}/edit" #path to edit page
+		visit "books/delete?id=#{@book.id}" #path to edit page
 		sleep(3)
 	
-		click_button "Update Book"
-		expect(page).to have_content("Romeo and Juliet")
+		click_button "Delete Book" #deletes book from the database
 		sleep(3)
+		expect(page).to have_content("deleted successfully")
+		
 	end
 
 
+	
 	#test double
+	
 	#create a mock test to allow double to recieve info from 
-	#entries already in the database
+	#entries already in the database 
 	it "confirms that a book entry is valid" do
-		@book = Book.new(title: "Harry Potter") #creates entry with title only
+		@book = Book.new(title: "Harry Potter", author: "JK Rowling", genre: "fiction", 
+						 price: "19.99", publishDate: "2000") #creates entry with title only
 		@book.save
 		visit books_index_path
 		sleep(3)
